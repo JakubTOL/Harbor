6249bc818d58dcb2b2652a2088ff4da578488cc3
Jakub Adamczyk <63499596+JakubTOL@users.noreply.github.com>
2026-05-19 19:37:15 +0200

    Enhance FinderColumnView with context menu functionality
    Added context menu support to FinderColumnView.

--- domains/explorer/widgets/finder_column_view.py
+++ domains/explorer/widgets/finder_column_view.py
@@ -1,11 +1,19 @@
 from PySide6.QtWidgets import QColumnView
-
+from PySide6.QtCore import Qt, QPoint
 
 class FinderColumnView(QColumnView):
 
-    def __init__(self, model, on_click, on_double_click):
+    def __init__(self, model, on_click, on_double_click, controller=None):
         super().__init__()
-
         self.setModel(model)
         # self.clicked.connect(on_click)
         self.doubleClicked.connect(on_double_click)
+        self.controller = controller
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.show_context_menu)
+
+    def show_context_menu(self, point: QPoint):
+        index = self.indexAt(point)
+        if not index.isValid() or not self.controller:
+            return
+        self.controller.show_context_menu(self, index, point)
