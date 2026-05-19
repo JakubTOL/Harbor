f548af372589c46d425afb3e3cd957453d4ac41a
Jakub Adamczyk <63499596+JakubTOL@users.noreply.github.com>
2026-05-19 18:32:53 +0200

    Change click event to double-click in ExplorerTree

--- domains/explorer/widgets/explorer_tree.py
+++ domains/explorer/widgets/explorer_tree.py
@@ -3,7 +3,7 @@ from PySide6.QtWidgets import QTreeView
 
 class ExplorerTree(QTreeView):
 
-    def __init__(self, model, on_click):
+    def __init__(self, model, on_double_click):
         super().__init__()
 
         self.setModel(model)
@@ -12,7 +12,7 @@ class ExplorerTree(QTreeView):
         for i in range(1, 4):
             self.hideColumn(i)
 
-        self.clicked.connect(on_click)
+        self.doubleClicked.connect(on_double_click)
         self.clicked.connect(self.expand_or_collapse_on_click)
 
     def expand_or_collapse_on_click(self, index):
