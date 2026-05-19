7c1c7cdb70dfac151d4a3139cf2d4ed73705700a
Jakub Adamczyk <63499596+JakubTOL@users.noreply.github.com>
2026-05-19 19:37:41 +0200

    Enhance ExplorerTree with context menu functionality
    Added context menu with actions for renaming, creating new files and folders, and deleting items. Included controller reference in ExplorerTree initialization.

--- domains/explorer/widgets/explorer_tree.py
+++ domains/explorer/widgets/explorer_tree.py
@@ -1,13 +1,14 @@
-from PySide6.QtWidgets import QTreeView
-
+from PySide6.QtWidgets import QTreeView, QMenu, QInputDialog, QMessageBox
+from PySide6.QtGui import QAction
+from PySide6.QtCore import QPoint, Qt
 
 class ExplorerTree(QTreeView):
 
-    def __init__(self, model, on_double_click):
+    def __init__(self, model, on_double_click, controller=None):
         super().__init__()
-
         self.setModel(model)
         self.setColumnWidth(0, 300)
+        self.controller = controller  # Add reference to controller
 
         for i in range(1, 4):
             self.hideColumn(i)
@@ -15,8 +16,38 @@ class ExplorerTree(QTreeView):
         self.doubleClicked.connect(on_double_click)
         self.clicked.connect(self.expand_or_collapse_on_click)
 
+        # Enable context menu
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.show_context_menu)
+
     def expand_or_collapse_on_click(self, index):
         if self.isExpanded(index):
             self.collapse(index)
         else:
             self.expand(index)
+
+    def show_context_menu(self, point: QPoint):
+        index = self.indexAt(point)
+        if not index.isValid():
+            return
+
+        menu = QMenu(self)
+
+        rename_action = QAction("Rename", self)
+        new_file_action = QAction("New File", self)
+        new_folder_action = QAction("New Folder", self)
+        delete_action = QAction("Delete", self)
+
+        # Connect actions
+        rename_action.triggered.connect(lambda: self.controller.rename_item(index))
+        new_file_action.triggered.connect(lambda: self.controller.create_new_file(index))
+        new_folder_action.triggered.connect(lambda: self.controller.create_new_folder(index))
+        delete_action.triggered.connect(lambda: self.controller.delete_item(index))
+
+        menu.addAction(rename_action)
+        menu.addAction(new_file_action)
+        menu.addAction(new_folder_action)
+        menu.addSeparator()
+        menu.addAction(delete_action)
+
+        menu.exec(self.viewport().mapToGlobal(point))
